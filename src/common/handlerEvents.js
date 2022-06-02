de407c3297dabaf285ba51f2208678089a4446a5
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-06-03 03:17:26 +0500

    deleted unusing commons

--- src/common/handlerEvents.js
+++
@@ -1,9 +0,0 @@
-/** Checking event on clicked 'enter' key */
-export const isEnterPressed = (ev) => {
-  let isEnter = false;
-  if (ev.key === "Enter") {
-    ev.preventDefault();
-    isEnter = true;
-  }
-  return isEnter;
-};
--- src/common/validations/checkData.js
+++
@@ -1,5 +0,0 @@
-import { mailReg } from "../../constants/regularExpressions";
-
-export const isEmail = (email) => {
-  return mailReg.test(email);
-};
