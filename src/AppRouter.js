268fb3ca5d8de38388be61ff531e86477da565be
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-06-07 14:25:30 +0500

    change BrowserRoute -> HashRouter for gh-pages

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,16 +1,16 @@
 import React from "react";
 import HomePage from "./pages/HomePage/HomePage";
 import NewBookPage from "./pages/NewBookPage/NewBookPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { basePath, newBookPath } from "./constants/routePath";
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path={basePath} element={<HomePage />} />
       <Route path={newBookPath} element={<NewBookPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRouter;
